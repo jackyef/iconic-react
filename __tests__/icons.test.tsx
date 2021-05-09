fe5974088bc18aef89ce15c26c1515914ee55160
jackyef <jacky.efendi1@gmail.com>
2021-05-09 21:15:33 +0700

    feat: Added tests

---
+++ __tests__/icons.test.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import fs from "fs";
+import path from "path";
+import { render } from "@testing-library/react";
+
+const componentsDir = path.resolve(__dirname, "../components");
+const components = fs.readdirSync(componentsDir);
+
+components.forEach((compFile) => {
+  const compName = compFile.replace(".tsx", "");
+
+  describe(`${compName} icon`, () => {
+    it("renders without error", () => {
+      const IconComponent = require(path.resolve(componentsDir, compFile))[
+        compName
+      ];
+
+      const { container } = render(<IconComponent />);
+
+      expect(container.querySelector("svg")).not.toBeNull();
+    });
+  });
+});
